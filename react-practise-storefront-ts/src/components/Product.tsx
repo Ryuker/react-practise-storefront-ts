ab32ff3a26352d58c27c2bbe51fb3d5b19fe267a
ryuker <lifeisagamenotaproduct@gmail.com>
2023-08-08 00:32:56 +0200

    moved details into h2

--- react-practise-storefront-ts/src/components/Product.tsx
+++ react-practise-storefront-ts/src/components/Product.tsx
@@ -7,8 +7,7 @@ interface Product {
 export default function Product({details} : {details: Product}){
   return (
     <>
-      <h2>Product {details.id}</h2>
-      <h3>{details.name}</h3>
+      <h2>Product {details.id} - {details.name}</h2>
       <span>{details.description}</span>
       <button>Add to Cart</button>
       <button>Remove from Cart</button>
