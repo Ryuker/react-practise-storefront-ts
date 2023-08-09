1a0e2cda789f3621eacf70abb6744f2f342ee69b
ryuker <lifeisagamenotaproduct@gmail.com>
2023-08-09 17:06:31 +0200

    added test buttons to test product add to cart in AppContext

--- react-practise-storefront-ts/src/components/CartContextDisplay.tsx
+++ react-practise-storefront-ts/src/components/CartContextDisplay.tsx
@@ -1,23 +1,43 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
-import { IProduct } from "./Product";
+import { Cheese, Chocolate, Milk, Yoghurt } from "./Inventory";
 
 
 export default function CartContextDisplay() {
   const app = useContext(AppContext);
 
-  const newProduct: IProduct = {
-    id: 1,
-    name: "Cheese",
-    description: "Lovely cheese" 
-  }
-
   return (
     <>
       <h2>cart context display</h2>
-      <span>{app.cart.length}</span>
-      <button onClick={() => app.onProductAdd(newProduct)}>Add New Product to Cart</button>
-      <button onClick={() => app.onProductDelete()}>Remove Product from Cart</button>
+      <span>{app.cart.length > 0 ? app.cart.length + " products in cart": "no product in cart"}</span>
+      {app.cart.length > 0 && 
+      <ul>
+        {app.cart.map(product => 
+          <li key={product.id}>
+            {`id: ${product.id} 
+            - name: ${product.name} 
+            - description: ${product.description} 
+            - quantity: ${product.quantity}
+            `}
+          </li>
+        )}
+      </ul>}
+      <div>
+        <button onClick={() => app.onProductAdd(Cheese)}>Add Cheese to Cart</button>
+        <button onClick={() => app.onProductDelete()}>Remove Product from Cart</button>
+      <div>
+      </div>
+        <button onClick={() => app.onProductAdd(Chocolate)}>Add Chocolate to Cart</button>
+        <button onClick={() => app.onProductDelete()}>Remove Product from Cart</button>
+      </div>
+      <div>
+        <button onClick={() => app.onProductAdd(Milk)}>Add Milk to Cart</button>
+        <button onClick={() => app.onProductDelete()}>Remove Product from Cart</button>
+      </div>
+      <div>
+        <button onClick={() => app.onProductAdd(Yoghurt)}>Add Yoghurt to Cart</button>
+        <button onClick={() => app.onProductDelete()}>Remove Product from Cart</button>
+      </div>
     </>
   );
 }
---
+++ react-practise-storefront-ts/src/components/Inventory.tsx
@@ -0,0 +1,25 @@
+import { IProduct } from "./Product";
+
+export const Cheese: IProduct = {
+  id: 1,
+  name: "Cheese",
+  description: "Lovely cheese"
+}
+
+export const Milk: IProduct = {
+  id: 2,
+  name: "Milk",
+  description: "Yummi milk"
+}
+
+export const Chocolate: IProduct = {
+  id: 3,
+  name: "Chocolate",
+  description: "Sweet chocolate"
+}
+
+export const Yoghurt: IProduct = {
+  id: 4,
+  name: "Yoghurt",
+  description: "Healthy yoghurt"
+}
