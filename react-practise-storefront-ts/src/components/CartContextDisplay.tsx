a422bff45476adbe40e16b81dfc3b0d48bff18d9
ryuker <lifeisagamenotaproduct@gmail.com>
2023-08-09 17:42:30 +0200

    added remove buttons to CartContextDisplay as well

--- react-practise-storefront-ts/src/components/CartContextDisplay.tsx
+++ react-practise-storefront-ts/src/components/CartContextDisplay.tsx
@@ -24,19 +24,23 @@ export default function CartContextDisplay() {
       </ul>}
       <div>
         <button onClick={() => app.onProductAdd(Cheese)}>Add Cheese to Cart</button>
-        <button onClick={() => app.onProductDelete(Cheese.id)}>Remove Product from Cart</button>
+        <button onClick={() => app.onProductDelete(Cheese.id)}>Delete Product from Cart</button>
+        <button onClick={() => app.onProductRemove(Cheese.id)}>Remove product from Cart</button>
       <div>
       </div>
         <button onClick={() => app.onProductAdd(Chocolate)}>Add Chocolate to Cart</button>
-        <button onClick={() => app.onProductDelete(Chocolate.id)}>Remove Product from Cart</button>
+        <button onClick={() => app.onProductDelete(Chocolate.id)}>Delete Product from Cart</button>
+        <button onClick={() => app.onProductRemove(Chocolate.id)}>Remove product from Cart</button>
       </div>
       <div>
         <button onClick={() => app.onProductAdd(Milk)}>Add Milk to Cart</button>
-        <button onClick={() => app.onProductDelete(Milk.id)}>Remove Product from Cart</button>
+        <button onClick={() => app.onProductDelete(Milk.id)}>Delete Product from Cart</button>
+        <button onClick={() => app.onProductRemove(Milk.id)}>Remove product from Cart</button>
       </div>
       <div>
         <button onClick={() => app.onProductAdd(Yoghurt)}>Add Yoghurt to Cart</button>
-        <button onClick={() => app.onProductDelete(Yoghurt.id)}>Remove Product from Cart</button>
+        <button onClick={() => app.onProductDelete(Yoghurt.id)}>Delete Product from Cart</button>
+        <button onClick={() => app.onProductRemove(Yoghurt.id)}>Remove product from Cart</button>
       </div>
     </>
   );
