86539f30bf11960e9a8a71f7b25e042d399c3cc2
ryuker <lifeisagamenotaproduct@gmail.com>
2023-08-10 13:25:35 +0200

    added buttons to product details

--- react-practise-storefront-ts/src/routes/ProductDetails.tsx
+++ react-practise-storefront-ts/src/routes/ProductDetails.tsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Inventory } from "../components/Inventory";
 import { useParams } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { AppContext } from "../AppContext";
+import CartContextDisplay from "../components/CartContextDisplay";
 
 export default function ProductDetails(){
+  const app = useContext(AppContext);
   const params = useParams();
   const id = params.id && parseInt(params.id);
   const existingProduct = Inventory.find((product) => product.id === id);
@@ -28,7 +32,22 @@ const productDetails = existingProduct && Object.entries(existingProduct).map( (
         <li key={index}>{entree}</li>
       )}
 
+      {existingProduct && 
+        <div>
+          <button onClick={() => app.onProductAdd(existingProduct)}>
+          Add {existingProduct.name} to cart
+          </button>
+          <button onClick={() => app.onProductDelete(existingProduct.id)}>
+            Delete{existingProduct.name} from cart
+          </button>
+          <button onClick={() => app.onProductRemove(existingProduct.id)}>
+            Remove {existingProduct.name} from cart
+          </button>
+        </div>
+      }
+
       <NavLink to="/store">Back to Store</NavLink>
+      <CartContextDisplay />
     </>
   )
 
